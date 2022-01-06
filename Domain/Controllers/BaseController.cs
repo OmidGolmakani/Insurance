6333a6ba4953697c267701cef54061fbb0a15640
Omid Golmakani <OmidGolmakani@outlook.com>
2022-01-06 23:18:07 +0330

    base controller

---
+++ Domain/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Domain.Controllers
+{
+    [Microsoft.AspNetCore.Cors.EnableCors("Insurance")]
+    [Route("api/[controller]")]
+    [Produces(typeof(JsonResult))]
+    [ApiController]
+    //[CustomAuthorizationFilter]
+    public class BaseController : ControllerBase
+    {
+    }
+}
