2537a9037c577df57a25ce50f476360cb57bef54
OmidGolmakani <46690215+OmidGolmakani@users.noreply.github.com>
2022-03-13 00:52:17 +0330

    Add  Insurance Title Requests Class

---
+++ Domain/Models/Dtos/Requests/InsuranceTitles/GetInsuranceTitles.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models.Dtos.Fundamentals.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Dtos.Requests.InsuranceTitles
+{
+    public class GetInsuranceTitles : GetsRequest
+    {
+        public decimal? Code { get; set; }
+        public int? ParentId { get; set; }
+        public byte? level { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool? Active { get; set; }
+    }
+}
